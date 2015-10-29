ff61f2bba107148d8520e732480807611c5d5bd1
Yuya Minami <yuya373@me.com>
2015-10-30 00:17:12 +0900

    add slack-user-list-update

--- slack-user.el
+++ slack-user.el
@@ -26,6 +26,7 @@
 
 (defvar slack-users)
 (defvar slack-user-name nil)
+(defconst slack-user-list-url "https://slack.com/api/users.list")
 
 (defun slack-user-find (id)
   (cl-find-if (lambda (user)
@@ -49,6 +50,25 @@
 (defun slack-user-names ()
   (mapcar (lambda (u) (plist-get u :name))
           slack-users))
+
+(defun slack-user-equal-p (a b)
+  (string= (plist-get a :id) (plist-get b :id)))
+
+(defun slack-user-list-update ()
+  (slack-request
+   slack-user-list-url
+   :params (list (cons "token" slack-token))
+   :success #'slack-user-on-list-update
+   :sync nil))
+
+(cl-defun slack-user-on-list-update (&key data &allow-other-keys)
+  (unless (plist-get data :ok)
+    (error "%s" data))
+  (let ((users (plist-get data :members)))
+    (slack-im-update-room-list users)))
+
+(defun slack-user-pushnew (user)
+  (cl-pushnew user slack-users :test #'slack-user-equal-p))
 
 (provide 'slack-user)
 ;;; slack-user.el ends here
