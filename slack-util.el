92b98ca59487749dd8fe91e296e10b35d96fcf20
Yuya Minami <yuya373@me.com>
2016-07-06 00:32:43 +0900

    remove eval-after-load

--- slack-util.el
+++ slack-util.el
@@ -55,24 +55,22 @@
                                    nil
                                  value)))))))
 
-(eval-after-load "company"
-  '(progn
-     (defun company-slack-backend (command &optional arg &rest ignored)
-       "Completion backend for slack chats.  It currently understands
+(defun company-slack-backend (command &optional arg &rest ignored)
+  "Completion backend for slack chats.  It currently understands
 @USER; adding #CHANNEL should be a simple matter of programming."
-       (interactive (list 'interactive))
-       (cl-case command
-         (interactive (company-begin-backend 'company-slack-backend))
-         (prefix (when (and (cl-find major-mode '(slack-mode slack-edit-message-mode))
-                            (looking-back "\\(\\W\\|^\\)@\\(\\w*\\)" (line-beginning-position)))
-                   (match-string 2)))
-         (candidates (remove-if-not
-                      (lambda (x)
-                        (string-prefix-p arg x))
-                      (mapcar #'first (slack-user-names slack-current-team))))
-         (meta (format "%s%s"
-                       (slack-user-presence-to-string (slack-user-find-by-name arg slack-current-team))
-                       arg))))))
+  (interactive (list 'interactive))
+  (cl-case command
+    (interactive (company-begin-backend 'company-slack-backend))
+    (prefix (when (and (cl-find major-mode '(slack-mode slack-edit-message-mode))
+                       (looking-back "\\(\\W\\|^\\)@\\(\\w*\\)" (line-beginning-position)))
+              (match-string 2)))
+    (candidates (remove-if-not
+                 (lambda (x)
+                   (string-prefix-p arg x))
+                 (mapcar #'first (slack-user-names slack-current-team))))
+    (meta (format "%s%s"
+                  (slack-user-presence-to-string (slack-user-find-by-name arg slack-current-team))
+                  arg))))
 
 (provide 'slack-util)
 ;;; slack-util.el ends here
