f94c7c22f9ba87df496e71cfad01ac1b84f5cd1e
Yuya Minami <yuya373@me.com>
2017-01-22 18:49:21 +0900

    add thread buffer

--- slack-thread.el
+++ slack-thread.el
@@ -25,7 +25,14 @@
 ;;; Code:
 
 (require 'eieio)
+(require 'lui)
 (require 'slack-message)
+
+(defvar lui-prompt-string "> ")
+(define-derived-mode slack-thread-mode lui-mode "Slack - Thread"
+  ""
+  (lui-set-prompt lui-prompt-string)
+  (setq lui-input-function 'slack-thread-message--send))
 
 (defclass slack-thread ()
   ((thread-ts :initarg :thread_ts :initform "")
@@ -36,6 +43,50 @@
    (replies :initarg :replies :initform '())
    (active :initarg :active :initform t)
    ))
+(defun slack-thread-buf-name (room thread-ts)
+  (format "%s %s - %s" (slack-room-buffer-name room) "Thread" thread-ts))
+
+(defun slack-thread-get-buffer-create (room team thread-ts)
+  (let* ((buf-name (slack-thread-buf-name room thread-ts))
+         (buf (get-buffer buf-name)))
+    (when buf
+      (with-current-buffer buf (kill-buffer))
+      (setq buf nil))
+    (unless buf
+      (setq buf (generate-new-buffer buf-name))
+      (with-current-buffer buf
+        (slack-thread-mode)
+        (slack-buffer-enable-emojify)
+        (goto-char lui-input-marker)
+        (set (make-local-variable 'thread-ts) thread-ts)
+        (set (make-local-variable 'slack-current-team-id) (oref team id))
+        (set (make-local-variable 'slack-current-room-id) (oref room id))
+        ;; (add-hook 'kill-buffer-hook 'slack-reset-room-last-read nil t)
+        (add-hook 'lui-pre-output-hook 'slack-buffer-buttonize-link nil t)))
+    buf))
+
+(defun slack-thread-show-messages ()
+  (interactive)
+  (let* ((line (thing-at-point 'line))
+         (team-id slack-current-team-id)
+         (team (and team-id (slack-team-find team-id)))
+         (room-id slack-current-room-id)
+         (room (and room-id team (slack-room-find room-id team)))
+         (ts (slack-get-ts)))
+    (if (or (not team) (not room) (not ts))
+        (error "Can't find: %s" (or (and (not team) "team")
+                                    (and (not room) "room")
+                                    (and (not ts) "ts"))))
+    (let ((thread (oref (slack-room-find-message room ts) thread))
+          (buf (slack-thread-get-buffer-create room team ts)))
+
+      (unless thread (error "Can't find thread"))
+
+      (with-current-buffer buf
+        (cl-loop for m in (slack-room-sort-messages (copy-sequence (oref thread messages)))
+                 do (slack-buffer-insert m team)))
+
+      (funcall slack-buffer-function buf))))
 
 (defmethod slack-thread-to-string ((m slack-message) team)
   (with-slots (thread) m
