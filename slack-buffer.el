b580a45136820d382d36d99c0f48f511eb745155
Yuya Minami <yuya373@me.com>
2017-04-24 17:49:11 +0900

    gather update process when buffer in current frame

--- slack-buffer.el
+++ slack-buffer.el
@@ -149,13 +149,14 @@
     (if buffer
         (progn
           (if (slack-buffer-in-current-frame buffer)
-              (slack-room-update-mark room team msg)
+              (progn
+                (slack-room-update-last-read room msg)
+                (slack-room-update-mark room team msg))
             (slack-room-inc-unread-count room))
+
           (if replace (slack-buffer-replace buffer msg)
-            (with-current-buffer buffer
-              (if (slack-buffer-in-current-frame buffer)
-                  (slack-room-update-last-read room msg))
-              (slack-buffer-insert msg team)))
+            (with-current-buffer buffer (slack-buffer-insert msg team)))
+
           (and slack-buffer-create-on-notify
                (slack-room-create-buffer-bg room team)))
       (slack-room-inc-unread-count room))))
