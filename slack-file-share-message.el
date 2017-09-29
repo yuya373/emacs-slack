bf2eb88c50c7c86cc3820d0da08242480757b997
Yuya Minami <yuya373@me.com>
2017-09-29 13:41:11 +0900

    notify only message changed

--- slack-file-share-message.el
+++ slack-file-share-message.el
@@ -72,12 +72,32 @@
 (defmethod slack-message-star-api-params ((m slack-file-share-message))
   (cons "file" (oref (oref m file) id)))
 
-(defmethod slack-message-changed--copy ((this slack-file-share-message) payload)
-  (let ((new-file (slack-file-create (plist-get payload :file)))
-        (base-file (oref this file)))
-    (oset base-file comments (oref new-file comments))
-    (oset base-file initial-comment (oref new-file initial-comment)))
-  (call-next-method))
+(defmethod slack-message-changed--copy ((this slack-file-share-message) other)
+  (let ((changed (call-next-method)))
+    (with-slots (file) this
+      (with-slots (comments initial-comment) file
+        (let ((new-comments (oref (oref other file) comments)))
+          (unless (or (eq (length comments)
+                          (length new-comments))
+                      (cl-find-if #'(lambda (c)
+                                      (let ((old (cl-find-if #'(lambda (o)
+                                                                 (slack-equalp o c))
+                                                             comments)))
+                                        (if old
+                                            (not (string= (oref old comment)
+                                                          (oref c comment)))
+                                          t)))
+                                  new-comments))
+            (setq comments new-comments)
+            (setq changed t)))
+        (let ((new-initial-comment (oref (oref other file) initial-comment)))
+          (when (or (and (not initial-comment) new-initial-comment)
+                    (and (and initial-comment new-initial-comment)
+                         (not (string= (oref initial-comment comment)
+                                       (oref new-initial-comment comment)))))
+            (setq initial-comment new-initial-comment)
+            (setq changed t)))))
+    changed))
 
 (provide 'slack-file-share-message)
 ;;; slack-file-share-message.el ends here
