45a82e3fc37932a1ad6bad2e5fbcbd8e98667ea4
Yuya Minami <yuya373@me.com>
2017-11-06 22:35:27 +0900

    disable lui-truncate in message buffer

--- slack-message-buffer.el
+++ slack-message-buffer.el
@@ -32,6 +32,7 @@
   (add-hook 'lui-pre-output-hook 'slack-add-face-lazy nil t)
   (add-hook 'lui-pre-output-hook 'slack-search-code-block nil t)
   (add-hook 'lui-post-output-hook 'slack-display-image t t)
+  (setq-local lui-max-buffer-size nil)
   )
 
 (defclass slack-message-buffer (slack-room-buffer)
