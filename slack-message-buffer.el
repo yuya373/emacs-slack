c08b873baae129905a792898fd3c58f2019894f4
Yuya Minami <yuya373@me.com>
2018-02-19 19:47:07 +0900

    add TODO

--- slack-message-buffer.el
+++ slack-message-buffer.el
@@ -36,6 +36,7 @@
   (add-hook 'lui-pre-output-hook 'slack-add-face-lazy nil t)
   (add-hook 'lui-pre-output-hook 'slack-search-code-block nil t)
   (add-hook 'lui-post-output-hook 'slack-display-image t t)
+  ;; TODO move to `slack-room-buffer' ?
   (cursor-sensor-mode)
   (setq-local lui-max-buffer-size nil)
   )
