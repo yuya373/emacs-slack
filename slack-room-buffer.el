abc29e87ae97f5ac74d3035c70ba907a6811d722
Yuya Minami <yuya373@me.com>
2018-11-19 19:48:27 +0900

    fix slack-action

--- slack-room-buffer.el
+++ slack-room-buffer.el
@@ -51,6 +51,12 @@
     (define-key keymap [mouse-1] #'slack-attachment-action-run)
     keymap))
 
+(defvar slack-action-keymap
+  (let ((keymap (make-sparse-keymap)))
+    (define-key keymap (kbd "RET") #'slack-action-run)
+    (define-key keymap [mouse-1] #'slack-action-run)
+    keymap))
+
 (defconst slack-message-delete-url "https://slack.com/api/chat.delete")
 (defconst slack-get-permalink-url "https://slack.com/api/chat.getPermalink")
 
@@ -323,6 +329,40 @@ Execute this function when cursor is on some message."
                   :success #'on-success))))
           (slack-if-let* ((url (oref action url)))
               (browse-url url))))))
+
+(defun slack-message-run-action ()
+  (interactive)
+  (slack-if-let* ((buffer slack-current-buffer)
+                  (room (oref buffer room))
+                  (ts (slack-get-ts))
+                  (message (slack-room-find-message room ts))
+                  (not-ephemeral-messagep (not (oref message is-ephemeral))))
+      (slack-buffer-execute-message-action buffer ts)))
+
+(defun slack-action-run ()
+  (interactive)
+  (slack-if-let* ((bot (get-text-property (point) 'bot))
+                  (payload (get-text-property (point) 'payload))
+                  (buffer slack-current-buffer)
+                  (team (oref buffer team)))
+      (let ((url "https://slack.com/api/chat.action")
+            (params (list (cons "bot" bot)
+                          (cons "payload" payload))))
+        (cl-labels
+            ((log-error (err) (format "Error: %s, URL: %s, PARAMS: %s"
+                                      err
+                                      url
+                                      params))
+             (on-success (&key data &allow-other-keys)
+                         (slack-request-handle-error
+                          (data "slack-action-run" #'log-error))))
+          (slack-request
+           (slack-request-create
+            url
+            team
+            :type "POST"
+            :params params
+            :success #'on-success))))))
 
 (provide 'slack-room-buffer)
 ;;; slack-room-buffer.el ends here
